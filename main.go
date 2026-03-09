9e6efcebbe64daf5e014940dcfe3039a916f4b0c
ashish <ashish.karan96@gmail.com>
2026-03-09 19:14:22 +0530

    Removed daemon. Preparing for Repl build.

--- main.go
+++ main.go
@@ -2,49 +2,18 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("usage: ork <daemon|discover|stop>")
-		os.Exit(1)
+	port := flag.Int("port", 9800, "udp/tcp port")
+	flag.Parse()
+
+	node := NewNode(*port)
+	if err := node.Start(); err != nil {
+		log.Fatal(err)
 	}
 
-	switch os.Args[1] {
-	case "daemon":
-		fs := flag.NewFlagSet("daemon", flag.ExitOnError)
-		port := fs.Int("port", 9800, "udp/tcp port")
-		fs.Parse(os.Args[2:])
-		d := NewDaemon(*port)
-		if err := d.Start(); err != nil {
-			log.Fatal(err)
-		}
-
-	case "discover":
-		resp, err := daemonCmd(CmdDiscover)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(resp.Peers) == 0 {
-			fmt.Println("no peers found")
-			return
-		}
-		fmt.Printf("found %d peer(s):\n", len(resp.Peers))
-		for _, p := range resp.Peers {
-			fmt.Printf("  %s @ %s:%d\n", p.ID, p.Addr, p.Port)
-		}
-
-	case "stop":
-		if _, err := daemonCmd(CmdStop); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("daemon stopped")
-
-	default:
-		fmt.Println("usage: ork <daemon|discover|stop>")
-		os.Exit(1)
-	}
+	// TODO: REPL goes here
+	select {}
 }
